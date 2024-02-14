8d2172fd264c6feab2a17abc5fb8ef9e55ba6af8
Shree Kottes <kottezh@gmail.com>
2024-02-14 22:43:43 +0530

    split to funcs

--- src/main.cpp
+++ src/main.cpp
@@ -7,53 +7,39 @@ extern "C" {
 #include <unistd.h>
 }
 
-struct MagInfo {
+const uint16_t GMASK = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
+
+struct winInfo {
     uint16_t width;
     uint16_t height;
     int8_t maxzoom;
 
-    MagInfo():width(148), height(148), maxzoom(4) {} /* Default */
+    winInfo():width(148), height(148), maxzoom(4) {} /* Default */
 
-    MagInfo(uint16_t width, uint16_t height, int8_t maxzoom) {
+    winInfo(uint16_t width, uint16_t height, int8_t maxzoom) {
         this->width = width;
         this->height = height;
         this->maxzoom = maxzoom;
     }
 };
 
-int main() {
-    MagInfo lens; 
-
-    xcb_connection_t *conn; 
-    xcb_screen_t *screen;
-    xcb_window_t win_id;
-
-    conn = xcb_connect(nullptr, nullptr);
-
-    if(!xcb_connection_has_error(conn)) {
-        std::cout << "X server connect: OK\n";
-    } else {
-        std::cerr << "X server connect: FAILED\n";
-        return 1;
-    }
-
-    screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
-    win_id = xcb_generate_id(conn);
-
-
+xcb_window_t create_new_window(xcb_connection_t *conn, xcb_screen_t *screen, winInfo &lens) {
     uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_OVERRIDE_REDIRECT;
     const uint32_t mask_values[] = {
         screen->white_pixel,
         1
     };
 
+    xcb_window_t window = xcb_generate_id(conn);
+    xcb_query_pointer_reply_t *query_reply = xcb_query_pointer_reply(conn, xcb_query_pointer(conn, screen->root), 0); 
+
     xcb_create_window(
         conn,
         screen->root_depth,
-        win_id,
+        window,
         screen->root,
-        0,
-        0,
+        query_reply->root_x - lens.width/2,
+        query_reply->root_y - lens.width/2,
         lens.width,
         lens.height,
         0, 
@@ -63,9 +49,10 @@ int main() {
         mask_values
     );
 
-    xcb_map_window(conn, win_id);
-    xcb_flush(conn);
+    return window;
+}
 
+void grab_pointer(xcb_connection_t *conn, xcb_screen_t *screen) {
     xcb_grab_pointer_cookie_t grab_cookie = xcb_grab_pointer(
         conn,
         false,
@@ -92,9 +79,9 @@ int main() {
         std::cerr << "xcb_grab returned NULL\n";
         exit(EXIT_FAILURE);
     }
+}
 
-    uint16_t GMASK = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y;
-
+void listen_for_pointer(xcb_connection_t *conn, xcb_window_t win_id) {
     bool run = true;
     xcb_generic_event_t *event;
     while(run) {
@@ -112,11 +99,13 @@ int main() {
             }
             case XCB_MOTION_NOTIFY: {
                 xcb_motion_notify_event_t *e = (xcb_motion_notify_event_t*)event;
+                uint32_t coords[] = {static_cast<uint32_t>(e->root_x) - 74, static_cast<uint32_t>(e->root_y) - 74};
+
                 xcb_configure_window(
                     conn,
                     win_id,
                     GMASK,
-                    (uint32_t[]){(uint32_t)e->event_x, (uint32_t)e->event_y}
+                    coords
                 );
                 xcb_flush(conn);
                 break; 
@@ -125,8 +114,35 @@ int main() {
                 std::cout << "Unknown event: " << (event->response_type & ~0x80) << "\n";
                 break;
         }
-        free(event);
+        delete event;
     }
+    xcb_ungrab_pointer(conn, XCB_CURRENT_TIME);
+}
+
+int main() {
+    winInfo lens; 
+
+    xcb_connection_t *conn; 
+    xcb_screen_t *screen;
+    xcb_window_t win_id;
+
+    conn = xcb_connect(nullptr, nullptr);
+
+    if(!xcb_connection_has_error(conn)) {
+        std::cout << "X server connect: OK\n";
+    } else {
+        std::cerr << "X server connect: FAILED\n";
+        return 1;
+    }
+
+    screen = xcb_setup_roots_iterator(xcb_get_setup(conn)).data;
+    win_id = create_new_window(conn, screen, lens);
+
+    xcb_map_window(conn, win_id);
+    xcb_flush(conn);
+
+    grab_pointer(conn, screen);
+    listen_for_pointer(conn, win_id);
 
     std::cout << "Closing connection\n";
     xcb_disconnect(conn);
